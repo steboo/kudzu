5bf28dcb59a1fd10b8ab11989d180b1225a79b50
olad <olad@clockwork>
2015-07-27 17:50:05 -0700

    Added Player with initial state of one goat

--- server/kudzu.js
+++ server/kudzu.js
@@ -1,21 +1,31 @@
 var kudzu = (function() {
+    function Player() {
+	this.goats = [new Goat()];
+    }
+
     function Goat() {
 	this.gender = (Math.random() > 0.5 ? "F" : "M");
 	this.maxHunger = 50 + (Math.random() * 50);
+	this.name = (this.gender == "M" ? "Billy" : "Nanny"); // TODO: expand possibilities
 	this.pickiness = 50 + (Math.random() * 50);
 	this.wanderLust = 0; // For future use
     };
 
     function Kudzu() {
 	this.desirability = 88;
+	this.name = "kudzu";
 	this.nourishment = 1;
 	this.prominence = 80;
     };
 
     function Knapweed() {
 	this.desirability = 87;
+	this.name = "knapweed";
 	this.nourishment = 0.8;
 	this.prominence = 80;
+    };
+
+    return {
+	Player: Player;
     }
-	
 })();
