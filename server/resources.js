901dcd53d9519477be1528238f8952230a120604
oladon <oladon+git@gmail.com>
2015-08-09 10:48:25 -0700

    Bones & Cloth
    Added bones and cloth as new resources

--- server/resources.js
+++ server/resources.js
@@ -2,55 +2,71 @@ var resources = (function() {
     var list = [
         /* Resources */
         {
+            name: "bones",
+            desirability: 0.05,
+            displayName: { singular: "bone",
+                           plural: "bones" },
+            minExplored: 40,
+            nourishment: 0.2,
+            prominence: 0.25
+        },{
             name: "bunnies",
             desirability: -25,
-            minExplored: 10,
             displayName: { singular: "baby bunny",
                            plural: "baby bunnies" },
+            minExplored: 10,
             nourishment: -25,
             prominence: 0.00001
+        },{
+            name: "cloth",
+            desirability: 0.4,
+            displayName: { singular: "scrap of cloth",
+                           plural: "scraps of cloth" },
+            minExplored: 25,
+            nourishment: 0.18,
+            prominence: 0.2
         },{
             name: "knapweed",
             desirability: 0.67,
             minExplored: 0,
             nourishment: 0.8,
-            prominence: 0.8
+            prominence: 1
         },{
             name: "kudzu",
             desirability: 0.68,
             minExplored: 0,
             nourishment: 1,
-            prominence: 0.8
+            prominence: 1
         },{
             name: "rocks",
             desirability: 0.02,
-            minExplored: 15, // Might need later adjustment
             displayName: { singular: "rock",
                            plural: "rocks" },
+            minExplored: 15, // Might need later adjustment
             nourishment: 0.01,
             prominence: 0.2
         },{
             name: "rubber",
             desirability: 0.21,
-            minExplored: 50, // Might need later adjustment
             displayName: { singular: "bit of rubber",
                            plural: "bits of rubber" },
+            minExplored: 50, // Might need later adjustment
             nourishment: 0.1,
             prominence: 0.1
         },{
             name: "tin",
             desirability: 0.3,
-            minExplored: 30, // Might need later adjustment
             displayName: { singular: "tin can",
                            plural: "tin cans" },
+            minExplored: 30, // Might need later adjustment
             nourishment: 0.01,
             prominence: 0.1
         },{
             name: "wood",
             desirability: 0.45,
-            minExplored: 5, // Might need later adjustment
             displayName: { singular: "piece of wood",
                            plural: "pieces of wood" },
+            minExplored: 5, // Might need later adjustment
             nourishment: 0.2,
             prominence: 0.28
         }
